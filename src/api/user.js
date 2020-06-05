58c50c6ec0ed84bae742d3aef578049a5b188a6b
zengyou123 <zengyou123@laiai.com>
2020-06-05 14:33:03 +0800

    first commit

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,9 @@
 import request from '@/utils/request'
 
 export function login(data) {
+  // console.log(JSON.parse(this.date) + '=======')
   return request({
-    url: '/vue-admin-template/user/login',
+    url: '/api/user/login',
     method: 'post',
     data
   })
@@ -10,7 +11,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    url: '/api/user/info',
     method: 'get',
     params: { token }
   })
@@ -18,7 +19,7 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    url: '/vue-admin-template/user/logout',
+    url: '/user/logout',
     method: 'post'
   })
 }
