769c01a4b2311494805f0e50cebe1056158a75a3
zengyou123 <zengyou123@laiai.com>
2020-06-11 15:32:51 +0800

    添加备注信息

---
+++ src/api/projectInfo.js
@@ -0,0 +1,62 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  // console.log(JSON.parse(this.date) + '=======')
+  return request({
+    url: '/api/admin/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/api/admin/info',
+    method: 'get'
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/api/admin/logout',
+    method: 'post'
+  })
+}
+
+export function getList(data, page, limit) {
+  return request({
+    url: '/api/admin/page?current=' + page + '&pageSize=' + limit,
+    method: 'post',
+    data
+  })
+}
+
+export function create(data) {
+  return request({
+    url: '/api/admin',
+    method: 'post',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: '/api/admin',
+    method: 'put',
+    data
+  })
+}
+
+export function changeState(id, state) {
+  return request({
+    url: '/api/admin/changeState?id=' + id + '&state=' + state,
+    method: 'post'
+  })
+}
+
+export function deleteById(id) {
+  return request({
+    url: '/api/admin/' + id,
+    method: 'delete'
+  })
+}
