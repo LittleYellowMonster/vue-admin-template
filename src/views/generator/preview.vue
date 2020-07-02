32722bd7608fe8e3a6b23df7787efdf7649aea31
zengyou123 <zengyou123@laiai.com>
2020-07-02 19:08:56 +0800

    实现代码预览功能

--- src/views/generator/preview.vue
+++ src/views/generator/preview.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>你好vue</div>
-  <!-- <el-tabs v-model="activeName" type="card">
-    <el-tab-pane v-for="item in data" :key="item.name" :lazy="true" :label="item.name" :name="item.name">
+  <el-tabs v-model="activeName" type="card">
+    <el-tab-pane v-for="item in list" :key="item.name" :lazy="true" :label="item.name" :name="item.name">
       <Java :value="item.content" :height="height" />
     </el-tab-pane>
-  </el-tabs>-->
+  </el-tabs>
 </template>
 
 <script>
-// import Java from '@/components/JavaEdit/index'
+import Java from '@/components/JavaEdit/index'
 import { generator } from '@/api/autoCode'
 export default {
-  name: 'Preview', /* components: { Java },*/
+  name: 'Preview',
+  components: { Java },
   data() {
     return {
-      data: null,
+      list: null,
       height: '',
       activeName: 'Entity',
       tableCode: {
@@ -29,8 +29,8 @@ export default {
     this.tableCode.tableName = tableName
     this.height = document.documentElement.clientHeight - 180 + 'px'
     generator(this.tableCode).then(data => {
-      this.data = data
-      console.log('data===' + data)
+      this.list = data.data
+      console.log('list===' + this.list)
     }).catch(() => {
       // this.$router.go(-1)
       console.log('获取失败')
